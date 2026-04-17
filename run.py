0d2886e7369edec1420cedc9334dd28168a77e22
Jakob Kessler <IMaugrenI@protonmail.com>
2026-04-17 13:57:27 +0000

    Add primary run.py entrypoint

---
+++ run.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""Primary local_case_organizer runtime entrypoint."""
+
+from local_case_organizer.cli import main
+
+
+if __name__ == "__main__":
+    raise SystemExit(main())
